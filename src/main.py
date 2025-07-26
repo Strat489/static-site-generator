6d21a0f3760507333c3fd5f73703e6923a200276
Strat489 <strat2510@gmail.com>
2025-07-26 22:15:30 +0300

    fix the pages..

--- src/main.py
+++ src/main.py
@@ -10,10 +10,14 @@ dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -22,7 +26,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
